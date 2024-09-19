8bf6ed30ed6cd594787c8097f009b41ddddc0543
AAM1337 <amacinov@gmail.com>
2024-09-19 18:24:44 +0700

    Проверено, все работает

--- app/src/main/java/com/example/laba_1/MainActivity.kt
+++ app/src/main/java/com/example/laba_1/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.laba_1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,39 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val EditTextSymbol=findViewById<EditText>(R.id.editTextText);
+        val Btn=findViewById<Button>(R.id.button2)
+
+        Btn.setOnClickListener{
+            val Symbol=EditTextSymbol.text.toString()
+
+            if(Symbol.all{it.isUpperCase() && it in 'A'..'Z'})
+            {
+                when(Symbol)
+                {
+                    "L"->{
+                        Toast.makeText(this,"Это согласные буквы",Toast.LENGTH_LONG).show()
+                    }
+                    "M"->{
+                        Toast.makeText(this,"Это согласные буквы",Toast.LENGTH_LONG).show()
+                    }
+                    "K"->{
+                        Toast.makeText(this,"Это согласные буквы",Toast.LENGTH_LONG).show()
+                    }
+                    "D"->{
+                        Toast.makeText(this,"Это согласные буквы",Toast.LENGTH_LONG).show()
+                    }
+                    else->
+                    {
+                        Toast.makeText(this,"Возможно, это гласные буквы",Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this,"Нужно написать заглавную латинскую букву",Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
